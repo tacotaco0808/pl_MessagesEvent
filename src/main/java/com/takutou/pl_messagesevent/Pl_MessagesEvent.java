c9eb05e3083d24a8c593320078b7bd326f37aff1
TakutoMatsui <takuto1488@gmail.com>
2024-06-28 17:10:14 +0900

    コマンド実行テスト

--- src/main/java/com/takutou/pl_messagesevent/Pl_MessagesEvent.java
+++ src/main/java/com/takutou/pl_messagesevent/Pl_MessagesEvent.java
@@ -8,6 +8,8 @@ public final class Pl_MessagesEvent extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("Hello World!");
+        getLogger().info("コミット共有テスト");
+        this.getCommand("kit").setExecutor(new CommandKit());
     }
 
     @Override
