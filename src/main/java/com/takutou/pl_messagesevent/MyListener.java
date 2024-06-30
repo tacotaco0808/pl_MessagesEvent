bed6afc78f28c307facf9213a7b8c6e75055a76c
takutou <takuto1488@gmail.com>
2024-06-30 17:39:42 +0900

    サーバー全体アナウンス機能の実装

--- src/main/java/com/takutou/pl_messagesevent/MyListener.java
+++ src/main/java/com/takutou/pl_messagesevent/MyListener.java
@@ -4,11 +4,22 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyListener implements Listener {
+
+
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
-        Bukkit.broadcastMessage(ChatColor.BLUE +event.getPlayer().getName()+ChatColor.WHITE+"さんJokenサーバーへようこそ！");
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(ChatColor.BLUE + event.getPlayer().getName() + ChatColor.WHITE + "さんJokenサーバーへようこそ！");
     }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(ChatColor.BLUE + event.getPlayer().getName() + ChatColor.DARK_RED + "さんおつかれさま!");
+    }
+
 }
--- src/main/java/com/takutou/pl_messagesevent/Pl_MessagesEvent.java
+++ src/main/java/com/takutou/pl_messagesevent/Pl_MessagesEvent.java
@@ -1,17 +1,94 @@
 package com.takutou.pl_messagesevent;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Pl_MessagesEvent extends JavaPlugin {
 
+    //変数
+    private String message ;
+    private long interval; //default時間間隔
+
     @Override
     public void onEnable() {
+
         // Plugin startup logic
         getLogger().info("Hello World!");
         getLogger().info("コミット共有テスト");
         this.getCommand("kit").setExecutor(new CommandKit());
+        loadMessage();
+        //　イベントリスナー
         getServer().getPluginManager().registerEvents(new MyListener(),this);
+
+        //コマンドハンドラー
+        /*アナウンスセット*/
+        this.getCommand("setannounce").setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+                if (args.length == 0) {
+                    commandSender.sendMessage("メッセージを入力してください。");
+                    return false;
+                }
+                // 入力されたメッセージを結合して保存
+                message = String.join(" ", args);
+                commandSender.sendMessage("アナウンスのメッセージが設定されました: " + message);
+                //設定ファイル保存
+                saveMessage();
+                return true;
+            }
+        });
+        /*アナウンス間隔セット*/
+        this.getCommand("setannouncesec").setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+                if(strings.length != 1){
+                    commandSender.sendMessage("時間間隔を秒単位で指定してください。");
+                    return false;
+                }
+                try{
+                    interval = Long.parseLong(strings[0])*20;
+                    commandSender.sendMessage("アナウンスの間隔を設定しました"+strings[0]+"秒");
+                    scheduleAnnouncerTask();
+                    saveMessage();
+                    return true;
+                }catch (NumberFormatException error) {
+                    commandSender.sendMessage("有効な数値を入力してください");
+                    return false;
+                }
+            }
+        });
+        scheduleAnnouncerTask();
     }
+
+    //定期処理
+    private void scheduleAnnouncerTask(){
+        Bukkit.getScheduler().cancelTasks(this);
+        Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+            @Override
+            public void run() {
+                Bukkit.broadcastMessage(ChatColor.DARK_AQUA+"【定期】:\n"+ message + "\n---------------");
+            }
+        },0L,interval);
+    }
+    //設定よみこみ
+    public void loadMessage(){
+        FileConfiguration config = getConfig();
+        message = config.getString("announcement-message","デフォルトのメッセージ");
+        interval = config.getLong("announcement-interval", 300); // デフォルトは5分
+    }
+    //設定を保存
+    public void saveMessage(){
+        FileConfiguration config = getConfig();
+        config.set("announcement-message",message);
+        config.set("announcement-interval", interval);
+        saveConfig();;
+    }
+
 
     @Override
     public void onDisable() {
